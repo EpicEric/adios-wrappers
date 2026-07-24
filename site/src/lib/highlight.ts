ca44281ad313b39f5aa68e9d8445ec1b30d6e7d2
Ohin Taylor <kazani@kazani.dev>
2026-07-24 05:18:29 -0500

    site: add custom Korora highlight.js syntax highlighter

--- site/src/lib/highlight.ts
+++ site/src/lib/highlight.ts
@@ -2,6 +2,40 @@ import hljs, { type HLJSApi } from 'highlight.js';
 import { Marked } from 'marked';
 import { markedHighlight } from 'marked-highlight';
 
+function kororaHighlighter(hljs: HLJSApi) {
+  return {
+    name: 'Korora Types',
+    aliases: ['korora', 'adios'],
+    contains: [
+      {
+        className: 'built_in',
+        begin: '[a-zA-Z]+(?=<)',
+        relevance: 1,
+      },
+      {
+        className: 'literal',
+        begin: '[a-zA-Z]+',
+        relevance: 0,
+        keywords: {
+          // Types from
+          // https://github.com/llakala/lladios/blob/main/korora/default.nix
+
+          // Primitive types
+          keyword:
+            'string str any never int float number bool null attrs list function path pathLike derivation type',
+        }
+      },
+      {
+        className: 'punctuation',
+        begin: '>|,|<',
+        relevance: 0,
+      }
+    ]
+  };
+}
+
+hljs.registerLanguage('korora', kororaHighlighter)
+
 export let highlighter = hljs;
 
 export const marked = new Marked(markedHighlight({
