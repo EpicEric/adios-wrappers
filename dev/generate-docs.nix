c114cedd489229a7c1f27f5c7b73b36c53d68fea
Eman Resu <elevenaka11@gmail.com>
2026-09-22 21:30:22 -0400

    dev/generate-docs: filter out default if it's a function

--- dev/generate-docs.nix
+++ dev/generate-docs.nix
@@ -1,9 +1,10 @@
 let
-  inherit (builtins) attrNames filter mapAttrs getFlake;
+  inherit (builtins) attrNames filter getFlake isFunction mapAttrs;
   filterAttrValues = pred: set: removeAttrs set (filter (name: !pred set.${name}) (attrNames set));
   keysToRemove = [
     "defaultFunc"
     "mergeFunc"
+    "default"
   ];
   flake = getFlake (toString ../.);
 
@@ -18,6 +19,8 @@ mapAttrs (_: wrapper: {
     removeAttrs option keysToRemove
     // {
       type = option.type.name;
+      # functions can't be serialized to JSON
+      ${if option ? default && !isFunction option.default then "default" else null} = option.default;
     }
   ) wrapper.options;
 }) filteredModules
